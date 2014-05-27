a066b0109cc607cd47487ff7f56c6266aed9c60c
Mike Schneider <mikes@bn.co>
2014-05-27 12:11:53 -0400

    Google changed their manifest version, updating to match

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 if(document.querySelector('html').getAttribute('manifest') ==
-    '/_/chrome/newtab/manifest?espv=210&ie=UTF-8') {
+    '/_/chrome/newtab/manifest?espv=2&ie=UTF-8') {
   document.querySelector('#lga img').setAttribute('style', 'visibility: hidden');
 }
