47f9e9f504af0cb63aa5fd817fab6c6d034fc687
Mike Schneider <mikeaschneider@gmail.com>
2015-08-09 08:25:02 -0400

    Remove conditional to support latest Chrome

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-if(document.querySelector('html').getAttribute('manifest') ==
-    '/_/chrome/newtab/manifest?espv=2&ie=UTF-8') {
-  document.querySelector('#lga img').setAttribute('style', 'visibility: hidden');
-}
+document.querySelector('#lga img').setAttribute('style', 'visibility: hidden');
